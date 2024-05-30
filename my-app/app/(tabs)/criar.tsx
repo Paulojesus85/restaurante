5a92ebaf17638dfb6bad0d428eafb077bc3e9319
Paulojesus85 <paulo.historia.usp@gmail.com>
2024-05-29 21:43:56 -0300

    aula2905

---
+++ my-app/app/(tabs)/criar.tsx
@@ -0,0 +1,8 @@
+
+export default function CriarScreen(){
+  return (
+    <>
+    Criar
+    </>
+  )
+}
---
+++ my-app/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+import { Text } from "react-native";
+
+export default function HomeScreen(){
+  const [data, setData] = useState([]);
+
+  return (
+    <>
+    <Text>Home</Text>
+    </>
+  )
+}
